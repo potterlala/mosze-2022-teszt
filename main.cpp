6497334613406568ff387a2f17563beb5dedc99e
potterlala <potterlala@gmail.com>
2022-09-13 19:28:12 +0200

    hibajavitasa

--- main.cpp
+++ main.cpp
@@ -4,17 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    //N_ELEMENTS eliras!
     int *b = new int[NELEMENTS];
+    //Szoveg " " koze!
     std::cout << '1-100 ertekek duplazasa'
+    //hianyzik a feltetel + leptetes: i < N_ELEMENTS; i++
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
+    //hianyzik a feltetel: i < N_ELEMENTS
     for (int i = 0; i; i++)
     {
+        //hianyzik az ertek kiiratasa: b[i];
         std::cout << "Ertek:"
     }    
+    //felesleges kiiratni
     std::cout << "Atlag szamitasa: " << std::endl;
+    //atlag double!
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
@@ -22,5 +29,6 @@ int main()
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //hianyzik a memoriafelszabaditas: delete[] b;
     return 0;
 }
